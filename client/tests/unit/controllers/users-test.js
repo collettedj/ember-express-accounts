1c0fb4bb65f276d0ea8eae4000130e2941f59ba6
David Collette <collettedj@gmail.com>
2015-05-10 21:46:59 -0700

    make the application look a little nicer

--- client/tests/unit/controllers/users-test.js
+++ client/tests/unit/controllers/users-test.js
@@ -11,5 +11,5 @@ moduleFor('controller:users', {
 // Replace this with your real tests.
 test('it exists', function(assert) {
   var controller = this.subject();
-  assert.ok(controller);
+  assert.ok(controller); 
 });
