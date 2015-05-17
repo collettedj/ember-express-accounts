cb98ff71e193b31fe8d18ad75e94cded2a46c7a9
David Collette <collettedj@gmail.com>
2015-05-16 21:02:19 -0700

    moved test fixture data to seperate json file

--- client/tests/acceptance/users-test.js
+++ client/tests/acceptance/users-test.js
@@ -1,62 +1,13 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from 'client/tests/helpers/start-app';
-// import { defineFixture } from 'ic-ajax';
-
-
-// defineFixture('/api/v1/auth/login', {
-//   response: {
-//     "_id":"554d96840b66257326b08b34",
-//     "firstName":"David",
-//     "email":"collettedj@gmial.com",
-//     "username":"collettedj",
-//     "__v":0,
-//     "lastName":"Collette",
-//     "age":36},
-//   jqXHR: {},
-//   textStatus: 'success'
-// });
+import usersData from './users-test-visit-users-data';
 
 $.mockjax({
     type:"GET",
     url:  "/api/v1/users",
     dataType: 'json',
-    responseText: {
-      "users":[
-          {"_id":"554d96840b66257326b08b34",
-            "firstName":"David",
-            "email":"collettedj@gmial.com",
-            "username":"collettedj",
-            "__v":0,
-            "lastName":"Collette",
-            "age":36
-          },
-          {"_id":"554ee22d35e086b2437e11e8",
-            "lastName":"Collette",
-            "firstName":"Reshma",
-            "email":"collettedj2@gmail.com",
-            "username":"collettedj2",
-            "__v":0,
-            "age":22
-          },
-          {"_id":"554ee30ac77991b844e64827",
-            "lastName":"Collette",
-            "firstName":"Andrew",
-            "email":"collettedj3@gmail.com",
-            "username":"collettedj3",
-            "__v":0,
-            "age":44
-          },
-          {"_id":"554ee3bac77991b844e64828",
-            "lastName":"Collette",
-            "firstName":"Adam",
-            "email":"collettedj5@gmail.com",
-            "username":"collettedj5",
-            "__v":0,
-            "age":65
-          }
-        ]
-      }
+    responseText: usersData
 });
 
 var application;
@@ -77,5 +28,8 @@ test('visiting /users', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/users');
+
+    var userRows = find('.user-row');
+    assert.equal(userRows.length, 4, "Number of users");
   });
 });
