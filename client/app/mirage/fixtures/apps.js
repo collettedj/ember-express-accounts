aa670e83987a4681289a4fc58ff8dd0434c7b7ba
David Collette <collettedj@gmail.com>
2015-05-19 22:13:50 -0700

    fix up roles template a little bit

--- client/app/mirage/fixtures/apps.js
+++ client/app/mirage/fixtures/apps.js
@@ -1,22 +1,27 @@
 export default [
       {
         "_id":"1",
-        "name":"App 1"
+        "name":"App 1",
+        "description": "decription1"
       },
       {
         "_id":"2",
-        "name":"App 2"
+        "name":"App 2",
+        "description": "decription2"
       },
       {
         "_id":"3",
-        "name":"App 3"
+        "name":"App 3",
+        "description": "decription3"
       },
       {
         "_id":"4",
-        "name":"App 4"
+        "name":"App 4",
+        "description": "decription4"
       },
       {
         "_id":"5",
-        "name":"App 5"
+        "name":"App 5",
+        "description": "decription5"
       }  
 ];
--- client/app/mirage/fixtures/roles.js
+++ client/app/mirage/fixtures/roles.js
@@ -1,22 +1,27 @@
 export default [
       {
         "_id":"1",
-        "name":"Role 1"
+        "name":"Role 1",
+        "description": "decription1"
       },
       {
         "_id":"2",
-        "name":"Role 2"
+        "name":"Role 2",
+        "description": "decription2"
       },
       {
         "_id":"3",
-        "name":"Role 3"
+        "name":"Role 3",
+        "description": "decription3"
       },
       {
         "_id":"4",
-        "name":"Role 4"
+        "name":"Role 4",
+        "description": "decription4"
       },
       {
         "_id":"5",
-        "name":"Role 5"
+        "name":"Role 5",
+        "description": "decription5"
       }  
 ];
