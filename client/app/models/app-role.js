08336a41fa8cb029b74c39561ae81c4851f6ab70
David Collette <collettedj@gmail.com>
2015-06-01 22:27:24 -0700

    generate ember model for app-role

--- client/app/models/app-role.js
+++ client/app/models/app-role.js
@@ -1,8 +1,15 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
- 	app: DS.belongsTo('app'),
- 	name: DS.attr('string'),
- 	description: DS.attr('string'),
+import customModel from './custom/app-role';
 
+var AppRole = DS.Model.extend({
+		app:DS.belongsTo('app'), 
+		name:DS.attr('string'), 
+		description:DS.attr('string'), 
+		createdAt:DS.attr('date'), 
+		updatedAt:DS.attr('date'), 
 });
+
+customModel(AppRole);
+
+export default AppRole;
