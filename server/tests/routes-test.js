86cdf1c31d6cc00fee76661e2c7e6d6f4ac268ad
David Collette <collettedj@gmail.com>
2015-06-05 22:19:33 -0700

    refactor routes tests

--- server/tests/routes-test.js
+++ server/tests/routes-test.js
@@ -7,6 +7,8 @@ var utils = require("../modelHelpers/utils.js");
 var app = require("../app");
 var models = app.get('models');
 
+
+
 after(function(done){
 	models.sequelize.close();
 	done();
@@ -14,42 +16,51 @@ after(function(done){
 
 
 describe("routes", function(){
-	beforeEach(function(done){
-		console.log('got here');
-		test.cleanDb(models, done);
-  	});
 
-  	afterEach(function(){
-  		//models.sequelize.close();
-  	})
 
-	describe("user route", function(){
-		it("GET /apps", function(done){
-			models.App.bulkCreate([{
-					name: "name1",
-					description: "description1"				
-				},{
-					name: "name2",
-					description: "description2"				
-				},{
-					name: "name3",
-					description: "description3"				
-				},{
-					name: "name4",
-					description: "description4"				
-				}])
+
+	describe("apps route", function(){
+		var appsSeedData = [{
+				name: "name1",
+				description: "description1"				
+			},{
+				name: "name2",
+				description: "description2"				
+			},{
+				name: "name3",
+				description: "description3"				
+			},{
+				name: "name4",
+				description: "description4"				
+			}];
+
+
+		beforeEach(function(done){
+			console.log('got here');
+			var promise = test.cleanDb(models)
 				.then(function(){
-					request(app)
-						.get('/api/v1/apps')
-						.expect(200)
-						.end(function(err,res){
-							assert.equal(null, err);
-							var jsonRes = JSON.parse(res.text);
-							assert.notStrictEqual(jsonRes.apps, undefined);
-							assert.equal(jsonRes.apps.length, 4);
-							done();
-						});
+					return models.App.bulkCreate(appsSeedData);
 				})
+
+			test.finish(promise, done);
+	  	});
+
+	  	afterEach(function(){
+	  		//models.sequelize.close();
+	  	})
+
+
+		it("GET /apps", function(done){
+			request(app)
+				.get('/api/v1/apps')
+				.expect(200)
+				.end(function(err,res){
+					assert.equal(null, err);
+					var jsonRes = JSON.parse(res.text);
+					assert.notStrictEqual(jsonRes.apps, undefined);
+					assert.equal(jsonRes.apps.length, 4);
+					done();
+				});
 		});
 
 		it("POST /apps", function(done){
@@ -72,17 +83,12 @@ describe("routes", function(){
 					assert.equal(app.name, "App1");
 					assert.equal(app.description, newApp.app.description);
 
-					models.App.findAll()
-						.then(function(apps){
-							var app0 = apps[0];
-							assert.equal(apps.length, 1);
-							assert.equal(app0.name, "App1");
-							assert.equal(app0.description, newApp.app.description);
+					models.App.findById(app.id)
+						.then(function(insertedApp){
+							assert.equal(insertedApp.name, "App1");
+							assert.equal(insertedApp.description, newApp.app.description);
 							done();
-							
 						}, done);
-
-				
 				});
 		});
 		
@@ -94,35 +100,38 @@ describe("routes", function(){
 	 			}
 	 		};
 
-			models.App.bulkCreate([{
-					name: "name1",
-					description: "description1"				
-				},{
-					name: "name2",
-					description: "description2"				
-				},{
-					name: "name3",
-					description: "description3"				
-				},{
-					name: "name4",
-					description: "description4"				
-				}])
-				.then(function(insertedApps){
-					request(app)
-						.put('/api/v1/apps/1')
-						.send(newApp)
-						.expect(200)
-						.end(function(err,res){
-							assert.equal(null, err);
-							var jsonRes = JSON.parse(res.text);
-							assert.equal(jsonRes.app.description, "this is the updated object");
-							// var jsonRes = JSON.parse(res.text);
-							// assert.notStrictEqual(jsonRes.apps, undefined);
-							// assert.equal(jsonRes.apps.length, 4);
-							done();
-						});
-				})
+			request(app)
+				.put('/api/v1/apps/1')
+				.send(newApp)
+				.expect(200)
+				.end(function(err,res){
+					assert.equal(null, err);
+					var jsonRes = JSON.parse(res.text);
+					assert.equal(jsonRes.app.description, "this is the updated object");
+					done();
+				});
 		});
+
+		// it("Delete /apps", function(done){
+	 // 		var newApp = {
+	 // 			app: {
+	 // 				id: 1, 
+	 // 				description: "this is the updated object", 
+	 // 			}
+	 // 		};
+
+
+		// 	request(app)
+		// 		.put('/api/v1/apps/1')
+		// 		.send(newApp)
+		// 		.expect(200)
+		// 		.end(function(err,res){
+		// 			assert.equal(null, err);
+		// 			var jsonRes = JSON.parse(res.text);
+		// 			assert.equal(jsonRes.app.description, "this is the updated object");
+		// 			done();
+		// 		});
+		// });
 
 	});
 
--- server/tests/testUtils.js
+++ server/tests/testUtils.js
@@ -2,14 +2,21 @@ process.env.NODE_ENV = 'test';
 var chalk = require('chalk');
 
 module.exports = {
-	cleanDb: function(models, done){
-		models.sequelize.sync({force: true, logging: false})
-		.then(function () {
+	cleanDb: function(models){
+		return models.sequelize.sync({force: true, logging: false})
+	}, 
+
+	finish: function(promise, done){
+		promise.then(function(){
 			done();
 		})
 		.catch(function(err){
-			console.log(chalk.red(err));	
+			test.logError(err);
 			done();
 		});
+	},
+
+	logError: function(message){
+		console.log(chalk.red(message));
 	}
 };
