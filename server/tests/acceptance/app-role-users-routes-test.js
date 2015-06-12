97a1c58896cadf91ad56f234a46b8e3119625203
David Collette <collettedj@gmail.com>
2015-06-12 10:53:25 -0700

    start adding test for app role users

---
+++ server/tests/acceptance/app-role-users-routes-test.js
@@ -0,0 +1,111 @@
+"use strict";
+
+var test = require('../testUtils');
+var assert = require("assert");
+var app = require("../../app");
+var models = app.get('models');
+var request = require("supertest").agent(app);
+var testData = require("./generators/app-role-users-routes-test-data").createTestData(models);
+
+test.afterAll(models);
+
+describe("routes", function(){
+	describe("appRoleUsers route", function(){
+
+		beforeEach(function(done){
+			test.cleanAndGenerateDb(models, testData, done);
+	  	});
+
+	  	it("Total Rows Generated", function(done){
+	  		models.AppRoleUser.findAll()
+	  			.then(function(appRoleUsers){
+	  				assert.equal(appRoleUsers.length, 22);
+	  				done();
+	  			})
+	  			.catch(done);
+	  	});
+
+		// it("GET One /appUsers", function(done){
+		// 	request.get('/api/v1/appUsers/1').expect(200)
+		// 		.end(function(err,res){
+		// 			assert.equal(null, err);
+		// 			var jsonRes = JSON.parse(res.text);
+		// 			assert.notStrictEqual(jsonRes['app-user'], undefined);
+		// 			assert.equal(jsonRes['app-user'].id, 1);
+		// 			done();
+		// 		});
+		// });	  	
+
+		// it("GET Many /appUsers By App Id", function(done){
+		// 	request.get('/api/v1/appUsers').query({appId:1}).expect(200)
+		// 		.end(function(err,res){
+		// 			assert.equal(null, err);
+		// 			var jsonRes = JSON.parse(res.text);
+		// 			var appUsers = jsonRes['app-users'];
+
+		// 			assert.notStrictEqual(appUsers, undefined);
+		// 			assert.equal(appUsers.length, 4);
+		// 			assert.equal(appUsers[0].appId, 1);
+		// 			assert.equal(appUsers[0].userId, 1);
+		// 			assert.equal(appUsers[1].appId, 1);
+		// 			assert.equal(appUsers[1].userId, 2);
+		// 			done();
+		// 		});
+		// });
+
+		// it("GET Many /appUsers By User Id", function(done){
+		// 	request.get('/api/v1/appUsers').query({userId:2}).expect(200)
+		// 		.end(function(err,res){
+		// 			assert.equal(null, err);
+		// 			var jsonRes = JSON.parse(res.text);
+		// 			var appUsers = jsonRes['app-users'];
+
+		// 			assert.notStrictEqual(appUsers, undefined);
+		// 			assert.equal(appUsers.length, 3);
+		// 			assert.equal(appUsers[0].appId, 1);
+		// 			assert.equal(appUsers[0].userId, 2);
+		// 			assert.equal(appUsers[1].appId, 2);
+		// 			assert.equal(appUsers[1].userId, 2);
+		// 			done();
+		// 		});
+		// });		
+
+		// it("POST /appUsers", function(done){
+		// 	request.post('/api/v1/appUsers').send(testData.newModel).expect(200)
+		// 		.end(function(err,res){
+		// 			var appUser = res.body["app-user"];
+		// 			assert.equal(null, err);
+		// 			assert.equal(appUser.appId, testData.newModel.appUser.appId);
+		// 			assert.equal(appUser.userId, testData.newModel.appUser.userId);
+
+		// 			models.AppUser.findById(appUser.id)
+		// 				.then(function(insertedAppUser){
+		// 					assert.equal(insertedAppUser.appId, testData.newModel.appUser.appId);
+		// 					assert.equal(insertedAppUser.userId, testData.newModel.appUser.userId);
+		// 					done();
+		// 				}, done);
+		// 		});
+		// });
+		
+		// it("PUT /appUsers", function(done){
+		// 	request.put('/api/v1/appUsers/1').send(testData.updateModel).expect(200)
+		// 		.end(function(err,res){
+		// 			assert.equal(null, err);
+		// 			var jsonRes = JSON.parse(res.text);
+		// 			assert.equal(jsonRes["app-user"].appId, testData.updateModel.appUser.appId);
+		// 			assert.equal(jsonRes["app-user"].userId, testData.updateModel.appUser.userId);
+		// 			done();
+		// 		});
+		// });
+
+		// it("DELETE /appUsers", function(done){
+		// 	request.delete('/api/v1/appUsers/1').expect(204)
+		// 		.end(function(err,res){
+		// 			assert.equal(null, err);
+		// 			done();
+		// 		});
+		// });
+
+	});
+
+});
---
+++ server/tests/acceptance/generators/app-role-users-routes-test-data.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var _ = require('lodash');
+var Promise = require('sequelize').Promise;
+
+var appRoleUserTemplateHash = {
+	appRoleId: 1,
+	appUserId: 1
+};
+
+function AppRoleUsersTestData(models, appRolesTestData, appUsersTestData){
+	this._appRolesTestData = appRolesTestData;
+	this._appUsersTestData = appUsersTestData;
+
+	this.dataTemplate = this.compileTemplates(appRoleUserTemplateHash);
+	this.models = models;
+	this.newModel = {
+		"appUser": this.generateAppRoleUser(4, 4)	
+	};
+	this.updateModel = {
+		"appUser": this.generateAppRoleUser(1, 1)
+	};
+}
+
+require('./test-data.js').mixin(AppRoleUsersTestData);
+
+AppRoleUsersTestData.prototype.generateAppRoleUser = function(appRoleId, appUserId){
+	var appRoleUser = this.generateOne(this.dataTemplate, null);
+	appRoleUser.appRoleId = appRoleId;
+	appRoleUser.appUserId = appUserId;
+	return appRoleUser;
+};
+
+AppRoleUsersTestData.prototype.generateAppRoleUsers = function(appRoleId, appUserIds){
+	var numRecords = appUserIds.length;
+	var appRoleUsers = this.generateData(this.dataTemplate, numRecords);
+	_.forEach(appRoleUsers, function(appRoleUser, index){
+			appRoleUser.appRoleId = appRoleId;
+			appRoleUser.appUserId = appUserIds[index];
+		});
+	return appRoleUsers;
+};
+
+AppRoleUsersTestData.prototype.seedDatabase = function(numRecords){
+	var self = this;
+	return self._appUsersTestData.seedDatabase(4)
+	.then(function(appUsers){
+		var appIds = _(appUsers).pluck('appId').uniq().value();
+		var appRoles =_.flatten([
+			self._appRolesTestData.generateAppRoles(4, appIds[0]),
+			self._appRolesTestData.generateAppRoles(3, appIds[1]),
+			self._appRolesTestData.generateAppRoles(2, appIds[2]),
+			self._appRolesTestData.generateAppRoles(1, appIds[3]),
+		]);
+		return Promise.props({
+				appUsers: appUsers,
+				appRoles: self.models.AppRole.bulkCreate(appRoles, {returning: true})
+		});
+	})
+	.then(function(result){
+		var appUserIds = _.pluck(result.appUsers, 'id');
+		var appRoleIds = _.pluck(result.appRoles, 'id');
+		var seedData = _.flatten([
+			self.generateAppRoleUsers(appRoleIds[0], appUserIds),
+			self.generateAppRoleUsers(appRoleIds[1], _.dropRight(appUserIds, 3)),
+			self.generateAppRoleUsers(appRoleIds[2], _.dropRight(appUserIds, 6)),
+			self.generateAppRoleUsers(appRoleIds[3], _.dropRight(appUserIds, 9)),
+		]);
+		return self.models.AppRoleUser.bulkCreate(seedData, {returning: true});
+	});
+};
+
+exports.createTestData = function(models){
+	var appRolesTestData = require("./app-roles-routes-test-data").createTestData(models);
+	var appUsersTestData = require("./app-users-routes-test-data").createTestData(models);
+	return new AppRoleUsersTestData(models, appRolesTestData, appUsersTestData);
+};
